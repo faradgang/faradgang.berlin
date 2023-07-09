ba844f6eee8feefd0eefb5fb52846609424b1cdb
chris <christoph.hohnerlein@digitalservice.bund.de>
2023-07-09 21:00:09 +0200

    feat: prepare i18n

---
+++ src/i18n/ui.ts
@@ -0,0 +1,22 @@
+export const ui = {
+  en: {
+    name: "English",
+    "nav.back": "Back",
+    "nav.help": "Help",
+    "nav.imprint": "Imprint",
+    "nav.privacy-policy": "Privacy Policy",
+    lastUpdate: "Last update",
+  },
+  de: {
+    name: "Deutsch",
+    "nav.back": "Zurück",
+    "nav.help": "Helfen",
+    "nav.imprint": "Impressum",
+    "nav.privacy-policy": "Datenschutzerklärung",
+    lastUpdate: "Stand",
+  },
+} as const;
+
+export type Language = keyof typeof ui;
+
+export const defaultLang = "de" satisfies Language;
