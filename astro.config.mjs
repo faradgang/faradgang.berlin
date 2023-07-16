d148c0f568faa022bed4dddda5d0e34ee72557bd
chris <christoph.hohnerlein@digitalservice.bund.de>
2023-07-16 21:35:55 +0200

    refactor: replace empty index with redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from "astro/config";
 import { loadEnv } from "vite";
+import { defaultLang } from "./src/i18n/ui";
 const { SITE, BASE } = loadEnv(import.meta.env.MODE, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE ?? "https://faradgang.berlin",
   base: BASE ?? "",
-  experimental: {
-    assets: true,
-  },
+  experimental: { assets: true, redirects: true },
+  redirects: { "/": `/${defaultLang}` },
 });
